1d49520aff1ec5f1930c2a970166b4c78df3e214
beepscore <support@beepscore.com>
2014-08-24 22:08:48 -0700

    Add HelloMoonFragment.java

---
+++ app/src/main/java/com/beepscore/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,9 @@
+package com.beepscore.android.hellomoon;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by stevebaker on 8/24/14.
+ */
+public class HelloMoonFragment extends Fragment {
+}
